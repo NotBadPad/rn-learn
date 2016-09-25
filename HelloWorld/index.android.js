c7687ea473deaf725689f9698d1c8c4ab84b09d4
NotBadPad <NotBadPad@users.noreply.github.com>
2016-09-25 21:36:44 +0800

    fix

--- HelloWorld/index.android.js
+++ HelloWorld/index.android.js
@@ -32,4 +32,38 @@ class LotsOfGreetings extends Component {
   }
 }
 
-AppRegistry.registerComponent('HelloWorld', () => LotsOfGreetings);
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    // 每1000毫秒对showText状态做一次取反操作
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    // 根据当前showText的值决定是否显示text内容
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class BlinkApp extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    );
+  }
+}
+
+
+AppRegistry.registerComponent('HelloWorld', () => BlinkApp);
